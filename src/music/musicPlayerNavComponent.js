eaf93e99760eff67cd43e93271714a7a2f55e73c
Nikolay Popov <mail@lifebits.ru>
2016-05-05 22:18:45 +0700

    add music-player mobile version

--- src/music/musicPlayerNavComponent.js
+++ src/music/musicPlayerNavComponent.js
@@ -6,7 +6,7 @@ export default {
     restrict: 'E',
     bindings: {},
     template: playerTemplate,
-    controller: ['$scope', function($scope) {
+    controller: ['MobileDetectService', '$scope', function(MobileDetectService, $scope) {
         this.openCustomPlaylist = false;
         this.editCustomPlaylistState = false;
 
@@ -17,6 +17,8 @@ export default {
         this.checkEditCustomPlaylistState = () => {
             this.editCustomPlaylistState = ( this.editCustomPlaylistState == false ) ? true : false;
         };
+
+        this.isMobile = MobileDetectService.any();
 
         this.aaa = () => {
             console.log($scope);
